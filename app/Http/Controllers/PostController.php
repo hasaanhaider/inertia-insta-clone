54cace13e2b606cbcd4683a35436079cc2051db7
hasaanhaider <bejantal456@gmail.com>
2025-04-16 23:18:31 +0500

    fix the file upload issue

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,21 +7,23 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    
 
     public function store(Request $request)
     {
         $request->validate([
             'post_image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
         ]);
+    
         $post_image = $request->file('post_image');
-        $path = $post_image->store('public/post_images');
+        $path = $post_image->store('post_images', 'public');
+    
         $post = Post::create([
             'user_id' => auth()->user()->id,
             'post_image' => $path,
             'post_description' => $request->post_description
         ]);
-
-        return redirect()->route('home')->with('success', 'Post created successfully.');
+    
+        return back()->with('success', 'Post created successfully.');
     }
+    
 }
